75c86cebe9c1b77f10cf9c93ca0fd57fa21d41a8
abinaya72 <167061753+abinaya72@users.noreply.github.com>
2024-05-01 13:04:28 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,51 @@
+
+
+
+function checkWeather() {
+ 
+    var cityName = document.getElementById("cityname").value;
+    var selectdate = document.getElementById("dateinput").value;
+    
+    
+    var apiKey = '97b5704e275440e5e558cfd4d4d423cc';
+   
+    
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+        
+            var weatherData = data.list.find(entry => {
+                var entrydate= entry.dt_txt.split(' ')[0].split('-');
+                var sdp=selectdate.split('-');
+                
+                return(
+                    entrydate[1] === sdp[1] &&
+                    entrydate[2] === sdp[2] &&
+                    entrydate[0] === sdp[0] 
+                );
+
+            });
+
+            if(weatherData)
+            {
+            var temperature = weatherData.main.temp;
+            var description = weatherData.weather[0].description;
+
+            
+
+            document.getElementById("temperature").innerText = temperature + '°C';
+            document.getElementById("cityname1").innerText =cityname.value;
+            document.getElementById("description").innerText ="Weather : "+ description;
+        }
+        else{
+            document.getElementById("temperature").innerText = "not available";
+
+        }
+    })
+        .catch(error => {
+            
+            document.getElementById("temperature").innerText='Error', error;
+        });
+}
+
